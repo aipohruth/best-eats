e05eae3873b29d1ceb44dcaf4e29d9246e145835
Ruth Aipoh <aipohruth@gmail.com>
2022-11-25 10:55:32 +0100

    fix:a bug

--- src/App.js
+++ src/App.js
@@ -12,12 +12,9 @@ function App() {
     <div >
     <Navbar />
     <Hero />
-<<<<<<< HEAD
     <HeadlineCard infos={infos} setInfos={setInfos} />
     <Food />
-=======
-    <HeadlineCard infos={infos} setInfos={setInfos}/>
->>>>>>> f53076ca8999c46fd726d8ce3a72cdae96f03b3b
+  
     </div>
   );
 }
