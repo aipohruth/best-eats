792abb2fdce81ab9b6d81b4777b436a043d0c60e
Ruth Aipoh <aipohruth@gmail.com>
2022-11-25 15:39:09 +0100

    updated style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='max-w-[1640px] mx-auto p-4'>
         <hr/>
         <div className='bg-orange-600 text-black text-center '>
-        <p className='inline-flex text-sm'>Made with <MdFavorite className='mx-2 mt-1' /> by melissa &copy;</p>
+        <p className='inline-flex text-sm'>Made with <MdFavorite className='mx-2 mt-1' /> by melissa &copy; 2022</p>
         </div>
     </div>
   )
